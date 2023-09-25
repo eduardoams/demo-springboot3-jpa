417f1b302472dbe9b3281a0aec39d04efb58de3f
eduardoams <eduardoa.ms14@gmail.com>
2023-09-25 08:17:32 -0300

    Service layer, component registration

--- src/main/java/com/softwaressilva/demo/resources/UserResource.java
+++ src/main/java/com/softwaressilva/demo/resources/UserResource.java
@@ -1,19 +1,32 @@
 package com.softwaressilva.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.softwaressilva.demo.entities.User;
+import com.softwaressilva.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Eduardo Souza", "eduardoa.ms14@gmail.com", "(11) 91234-5678", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 }
