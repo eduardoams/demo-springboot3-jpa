2536c2a0ff92f727f9ef9bb370cab7686a28da37
eduardoams <eduardoa.ms14@gmail.com>
2023-09-27 22:53:43 -0300

    Product

--- src/main/java/com/softwaressilva/demo/config/TestConfig.java
+++ src/main/java/com/softwaressilva/demo/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.softwaressilva.demo.entities.Category;
 import com.softwaressilva.demo.entities.Order;
+import com.softwaressilva.demo.entities.Product;
 import com.softwaressilva.demo.entities.User;
 import com.softwaressilva.demo.entities.enums.OrderStatus;
 import com.softwaressilva.demo.repositories.CategoryRepository;
 import com.softwaressilva.demo.repositories.OrderRepository;
+import com.softwaressilva.demo.repositories.ProductRepository;
 import com.softwaressilva.demo.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,7 +41,14 @@ public class TestConfig implements CommandLineRunner {
 		Category c2 = new Category(null, "Computers");
 		Category c3 = new Category(null, "Books");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.5, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
 		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		User u1 = new User(null, "Amanda", "amanda@gmail.com", "999999999", "123456");
 		User u2 = new User(null, "Chloe", "chloe@gmail.com", "888888888", "123456");
--- src/main/java/com/softwaressilva/demo/resources/ProductResource.java
+++ src/main/java/com/softwaressilva/demo/resources/ProductResource.java
@@ -9,24 +9,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.softwaressilva.demo.entities.Category;
-import com.softwaressilva.demo.services.CategoryService;
+import com.softwaressilva.demo.entities.Product;
+import com.softwaressilva.demo.services.ProductService;
 
 @RestController
-@RequestMapping(value = "categories")
-public class CategoryResource {
+@RequestMapping(value = "products")
+public class ProductResource {
 	
 	@Autowired
-	private CategoryService service;
+	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Category> findById(@PathVariable Long id) {
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(service.findById(id));
 	}
 }
